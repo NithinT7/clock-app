3525b22b452f7cceaac5e6e6a53cf29928ec59a2
NithinT7 <90013428+NithinT7@users.noreply.github.com>
2023-07-06 23:10:25 +0530

    Finished Stopwatch implementation

---
+++ script.js
@@ -0,0 +1,42 @@
+const stopWatchStart = document.querySelector('.stopwatch #start');
+const stopWatchStop = document.querySelector('.stopwatch #stop');
+const stopWatchReset = document.querySelector('.stopwatch #reset');
+const lapBtn = document.querySelector('.stopwatch #lap');
+const stopWatch = document.querySelector('.stopwatch #stopwatch');
+
+const lap = document.querySelector('.laps #laps');
+const time = document.querySelector('.current-time');
+setInterval(() => {
+    let date = new Date();
+    time.innerHTML = date.toLocaleTimeString().slice(0, 5) + ' ' + date.toLocaleTimeString().slice(9, 11);
+}, 1000);
+let started = false;
+let intervalId = null;
+let count = 0;
+stopWatchStart.addEventListener('click', () => {
+    if (started) return;
+    started = true;
+    intervalId = setInterval(() => {
+        count++;
+        stopWatch.value = count + "ms";
+    }, 1);
+});
+
+stopWatchStop.addEventListener('click', () => {
+    started = false;
+    clearInterval(intervalId);
+});
+
+stopWatchReset.addEventListener('click', () => {
+    started=false;
+    clearInterval(intervalId);
+    stopWatch.value = "0ms";
+    count = 0;
+    lap.innerHTML = "";
+});
+
+lapBtn.addEventListener('click', () => {
+    const lapLi = document.createElement('li');
+    lapLi.innerHTML = stopWatch.value;
+    lap.appendChild(lapLi);
+});
